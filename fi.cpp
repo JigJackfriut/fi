d5c97d131fd070dd013827f2e14b42a02950cd38
JigJackfriut <124086458+JigJackfriut@users.noreply.github.com>
2023-05-08 15:59:29 -0400

    Update fi.cpp

--- fi.cpp
+++ fi.cpp
@@ -1,1 +1,77 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <nlohmann/json.hpp>
 
+
+#include "httplib.h"
+#include "dynamicArtDB.h"
+#include "dynamicArtInfo.h"
+
+using namespace httplib;
+using namespace std;
+using json = nlohmann::json;
+
+const int port = 5005;
+
+dynamicArtDB obj; // Create a global instance of the dynamicArtDB class
+
+int main() {
+    httplib::Server svr;
+
+  svr.Get(R"(/dynamicArt/getEmotionPercentages/)", [&](const Request& req, Response& res) {
+    res.set_header("Access-Control-Allow-Origin", "*");
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <nlohmann/json.hpp>
+
+
+#include "httplib.h"
+#include "dynamicArtDB.h"
+#include "dynamicArtInfo.h"
+
+using namespace httplib;
+using namespace std;
+using json = nlohmann::json;
+
+const int port = 5005;
+
+dynamicArtDB obj; // Create a global instance of the dynamicArtDB class
+
+int main() {
+    httplib::Server svr;
+
+  svr.Get(R"(/dynamicArt/getEmotionPercentages/)", [&](const Request& req, Response& res) {
+    res.set_header("Access-Control-Allow-Origin", "*");
+
+
+    dynamicArtDB dDB;
+    json result;
+
+ 
+   result = dDB.getEmotionCount();
+         
+  
+   res.set_content(result.dump(), "application/json");
+});
+
+svr.listen("0.0.0.0", port);
+    return 0;
+}
+    json result;
+
+ 
+        result = dDB.getEmotionCount();
+         
+  
+   res.set_content(result.dump(), "application/json");
+});
+
+svr.listen("0.0.0.0", port);
+    return 0;
+}
