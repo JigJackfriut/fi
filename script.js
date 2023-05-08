1fd8f7d6563f99d78f48f42ea9923f10e8013796
JigJackfriut <124086458+JigJackfriut@users.noreply.github.com>
2023-05-08 16:06:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-const emotionData = {
-  "Happiness": 24,
-  "Disgust": 15,
-  "Fear": 10,
-  "Surprise": 5,
-  "Anger": 20,
-  "excited": 15
-};
+const baseUrl = 'http://54..198.38.17';
+const url = baseUrl + '/dynamicArt/getEmotionPercentages/';
+
+
 
 const colr = {
   "Happiness": "yellow",
   "Surprise": "pink",
   "Disgust": "green",
-  "Fear": "dark red","Anger":"blue","calm":"white"
+  "Fear": "dark red",
+  "Anger": "blue",
+  "Calm": "white" // corrected the capitalization of "Calm"
 };
 
 window.onload = function() {
@@ -25,72 +23,78 @@ window.onload = function() {
 
   const ctx = canvas.getContext('2d');
 
+  fetch(baseUrl+'/dynamicArt/getEmotionPercentages/', {
+    method: 'get'
+  })
+  .then(response => response.json())
+  .then(emotionData => { // corrected the variable name to emotionData
+    const word_attr = createWordData(emotionData);
 
-  const word_attr = createWordData(emotionData);
-
-  function animation() {
-    for (let i = 0; i < word_attr.length; i++) {
-      const word = word_attr[i];
-      ctx.font = word.font;
-      ctx.fillStyle = word.color;
-      ctx.fillText(word.text, word.x, word.y);
-      word.width = ctx.measureText(word.text).width;
-      ctx.stroke();
-    }
-    move();
-  }
-
-  function move() {
-    for (let i = 0; i < word_attr.length; i++) {
-      const word = word_attr[i];
-      if (word.x > w) {
-        word.x = -word.width;
-        word.y = Math.random() * h;
-      } else {
-        word.x += word.speed;
+    function animation() {
+      for (let i = 0; i < word_attr.length; i++) {
+        const word = word_attr[i];
+        ctx.font = word.font;
+        ctx.fillStyle = word.color;
+        ctx.fillText(word.text, word.x, word.y);
+        word.width = ctx.measureText(word.text).width;
+        ctx.stroke();
       }
+      move();
     }
-  }
 
-  setInterval(function() {
-    ctx.clearRect(0, 0, w, h);
-    animation();
-  }, 24);
-
-  function createWordData(emotionData) {
-    const words = [];
-    for (const emotion in emotionData) {
-      const count = emotionData[emotion];
-      for (let i = 0; i < count; i++) {
-        const word = criteria(emotion);
-        if (!overlapping(word.x, word.y, word.width, words)) {
-          words.push(word);
+    function move() {
+      for (let i = 0; i < word_attr.length; i++) {
+        const word = word_attr[i];
+        if (word.x > w) {
+          word.x = -word.width;
+          word.y = Math.random() * h;
+        } else {
+          word.x += word.speed;
         }
       }
     }
-    return words;
-  }
 
-  function criteria(key) {
-    const word = {};
-    word.text = key;
-    word.x = Math.random() * w;
-    word.y = Math.random() * h;
-    word.font = emotionData[key] * 5 + 'px Arial';
-    word.speed = emotionData[key] / 10;
-    word.color = colr[key];
-    word.width = 0;
-    return word;
-  }
+    setInterval(function() {
+      ctx.clearRect(0, 0, w, h);
+      animation();
+    }, 24);
 
-  function overlapping(x, y, size, placedWords) {
-    for (let i = 0; i < placedWords.length; i++) {
-      const word = placedWords[i];
-      const distance = Math.sqrt((x - word.x) ** 2 + (y - word.y) ** 2);
-      if (distance < (size + word.width) / 2) {
-        return true;
+    function createWordData(emotionData) {
+      const words = [];
+      for (const emotion in emotionData) {
+        const count = emotionData[emotion];
+        for (let i = 0; i < count; i++) {
+          const word = criteria(emotion);
+          if (!overlapping(word.x, word.y, word.width, words)) {
+            words.push(word);
+          }
+        }
       }
+      return words;
     }
-    return false;
-  }
+
+    function criteria(key) {
+      const word = {};
+      word.text = key;
+      word.x = Math.random() * w;
+      word.y = Math.random() * h;
+      word.font = emotionData[key] * 5 + 'px Arial';
+      word.speed = emotionData[key] / 10;
+      word.color = colr[key];
+      word.width = 0;
+      return word;
+    }
+
+    function overlapping(x, y, size, placedWords) {
+      for (let i = 0; i < placedWords.length; i++) {
+        const word = placedWords[i];
+        const distance = Math.sqrt((x - word.x) ** 2 + (y - word.y) ** 2);
+        if (distance < (size + word.width) / 2) {
+          return true;
+        }
+      }
+      return false;
+    }
+  });
 };
+
